e271c2041b3d1487538e0a6f088e0486d31992d7
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-02 21:21:41 +0200

    Add wall and ground screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AdminScreen } from "../sceens/admin/AdminScreen"
 import './App.css'
+import { WallScreen } from '../sceens/wall/WallScreen';
+import { GroundScreen } from '../sceens/ground/GroundScreen';
 
 function App() {
   return (
-    <>
-      <Router>        
+    <Router>        
           <div className="app">
           <Routes>
           <Route path="/" element={ <AdminScreen /> } />
+          <Route path="/wall" element={ <WallScreen /> } />
+          <Route path="/ground" element={ <GroundScreen /> } />
           </Routes>
           </div>
 
       </Router>
-    </>
   );
 }
 
