a3efa21960192b10c50e62ec8adf57a4b45adfde
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-03 19:10:28 +0200

    Map BattlemapsColumn and MapElements  generic

--- src/components/MapOverview.tsx
+++ src/components/MapOverview.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 interface MapOverviewProps {
     gap: string,
     src: string
+    handleSceneSelection(id: number): number
 }
 
 const ContainerBattlemaps = styled.div`
@@ -26,34 +27,60 @@ const BattlemapsColumn = styled.div`
     gap: 2px;
 `;
 
-const MapOverview: FunctionComponent<MapOverviewProps> = ({ gap, src }): ReactElement => {
-    return(
-            <ContainerBattlemaps  style={{ gap: gap }}>
-                <BattlemapsColumn style={{ gap: gap }}>
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
+const MapOverview: FunctionComponent<MapOverviewProps> = ({ gap, src, handleSceneSelection }): ReactElement => {
+
+    interface MockItem {
+        id: number;
+    }
+    
+    interface MockData {
+        items: MockItem[];
+    }
+
+    const Mock: MockData  = {
+        "items": [
+          { "id": 1 },
+          { "id": 2 },
+          { "id": 3 },
+          { "id": 4 },
+          { "id": 5 },
+          { "id": 6 },
+          { "id": 7 },
+          { "id": 8 },
+          { "id": 9 },
+          { "id": 10 },
+          { "id": 11 },
+          { "id": 12 },
+          { "id": 13 },
+          { "id": 14 },
+          { "id": 15 },
+          { "id": 16 }
+        ]
+      }
+      
+      const count = Math.sqrt(Mock.items.length);
+      
+      return (
+        <ContainerBattlemaps style={{ gap: gap }}>
+            {[...Array(count)].map((_, colIndex) => (
+                <BattlemapsColumn style={{ gap: gap }} key={colIndex}>
+                    {[...Array(count)].map((_, mapIndex) => {
+                        const itemIndex = colIndex * count + mapIndex;
+                        if (itemIndex < Mock.items.length) {
+                            return (
+                                <MapElement 
+                                    src={src} 
+                                    handleSceneSelection={handleSceneSelection} 
+                                    key={Mock.items[itemIndex].id}
+                                    keyProp={Mock.items[itemIndex].id}>    
+                                </MapElement>
+                            );
+                        }
+                        return null;
+                    })}
                 </BattlemapsColumn>
-                <BattlemapsColumn style={{ gap: gap }}>
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                </BattlemapsColumn>
-                <BattlemapsColumn style={{ gap: gap }}>
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                </BattlemapsColumn>
-                <BattlemapsColumn style={{ gap: gap }}>
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                    <MapElement src={ src } />
-                </BattlemapsColumn>
-            </ContainerBattlemaps>
-    )
+            ))}
+        </ContainerBattlemaps>
+    );
 }
 export { MapOverview }
