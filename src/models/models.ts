f224f2876c7339784dd93af0de52c38ebfe460f0
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-05 21:17:22 +0200

    Add battlemap model

---
+++ src/models/models.ts
@@ -0,0 +1,10 @@
+export interface Battlemap {
+  xp: string;
+  loot: string;
+  enemies: string;
+  locked: boolean;
+  source_locked: string;
+  id: number;
+  name: string;
+  source_clear: string;
+}
