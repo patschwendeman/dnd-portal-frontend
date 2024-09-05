e5b08cfcf1e62c08380e1e91107219d09256e7b5
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-05 18:37:25 +0200

    Add context and local storage to handle state from different screens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AdminScreen } from "../sceens/AdminScreen"
 import { WallScreen } from '../sceens/WallScreen';
 import { GroundScreen } from '../sceens/GroundScreen';
+import { ActiveSceneContext } from './context/context';
 
 function App() {
-  return (
-    <Router>        
-          <div className="app">
-          <Routes>
-          <Route path="/" element={ <AdminScreen /> } />
-          <Route path="/wall" element={ <WallScreen /> } />
-          <Route path="/ground" element={ <GroundScreen /> } />
-          </Routes>
-          </div>
+  const [activeScene, setActiveScene] = useState<number>(() => {
+    const savedScene = localStorage.getItem('activeScene');
+    return savedScene ? parseInt(savedScene, 10) : 1; 
+  });
 
+  useEffect(() => {
+    localStorage.setItem('activeScene', activeScene.toString());
+  }, [activeScene]);
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'activeScene') {
+        const newActiveScene = event.newValue ? parseInt(event.newValue, 10) : 1;
+        setActiveScene(newActiveScene);
+      }
+    };
+    window.addEventListener('storage', handleStorageChange);
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  return (
+    <ActiveSceneContext.Provider value={{ activeScene, setActiveScene }}>
+      <Router>      
+        <div className="app">
+          <Routes>
+            <Route path="/" element={ <AdminScreen /> } />
+            <Route path="/wall" element={ <WallScreen /> } />
+            <Route path="/ground" element={ <GroundScreen /> } />
+          </Routes>
+        </div>
       </Router>
+    </ActiveSceneContext.Provider>
   );
 }
 
-export default App
+export default App;
