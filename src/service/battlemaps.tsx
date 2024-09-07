1e2a3a67559a28d1034e51c6884edb7bdb178f15
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-07 12:37:14 +0200

    Add battlemapLight model

--- src/service/battlemaps.tsx
+++ src/service/battlemaps.tsx
@@ -1,7 +1,14 @@
 import { getData } from "../api/apiMethods";
-import { Battlemap } from "../models/models"
+import { Battlemap, BattlemapLight } from "../models/models"
 
 export const getBattlemaps = async (setBattlemaps: React.Dispatch<React.SetStateAction<Battlemap[]>>) => {
     const battlemaps =  await getData('battlemaps/');
     setBattlemaps(battlemaps);
+}
+
+export const getBattlemapsfiltered = async (setBattlemaps: React.Dispatch<React.SetStateAction<BattlemapLight[]>>, params: { [key: string]: any }) => {
+    console.log(params)
+    const battlemaps =  await getData('battlemaps/', params);
+    
+    //setBattlemaps(battlemaps);
 }
