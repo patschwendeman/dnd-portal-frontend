ae96ac5f5a2c4e216fb4f1a4fb9c0971f3564d6e
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-07 16:16:44 +0200

    Add default maps to display grit if maps are undefined

--- src/service/battlemaps.tsx
+++ src/service/battlemaps.tsx
@@ -1,14 +1,12 @@
 import { getData } from "../api/apiMethods";
-import { Battlemap, BattlemapLight } from "../models/models"
+import { Map } from "../models/models"
 
-export const getBattlemaps = async (setBattlemaps: React.Dispatch<React.SetStateAction<Battlemap[]>>) => {
+export const getBattlemaps = async (setBattlemaps: React.Dispatch<React.SetStateAction<Map[]>>) => {
     const battlemaps =  await getData('battlemaps/');
     setBattlemaps(battlemaps);
 }
 
-export const getBattlemapsfiltered = async (setBattlemaps: React.Dispatch<React.SetStateAction<BattlemapLight[]>>, params: { [key: string]: any }) => {
-    console.log(params)
+export const getBattlemapsfiltered = async (setBattlemaps: React.Dispatch<React.SetStateAction<Map[]>>, params: { [key: string]: boolean }) => {
     const battlemaps =  await getData('battlemaps/', params);
-    
-    //setBattlemaps(battlemaps);
+    setBattlemaps(battlemaps);
 }
