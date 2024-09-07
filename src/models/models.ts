ff1cb0c56c210603c9677279ac3e58413db1a1fb
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-09-07 19:20:21 +0200

    Add music, screen and sceneDetail models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,15 +1,42 @@
 export interface Battlemap {
-  xp: string;
-  loot: string;
-  enemies: string;
-  locked: boolean;
-  source_locked: string;
-  id: number;
-  name: string;
-  source_clear: string;
+  xp: string
+  loot: string
+  enemies: string
+  locked: boolean
+  source_locked: string
+  id: number
+  name: string
+  source_clear: string
 }
 
 export interface Map {
-  id: number;
-  source?: string;
+  id: number
+  source?: string
+}
+
+export interface Music {
+  name: string
+  source: string
+  id: number
+}
+
+export interface Screen {
+  name: string
+  source: string
+  id: number
+}
+
+export interface SceneDetail {
+  name: string
+  graphics_wall_id: number
+  graphics_ground_id: number
+  battlemaps_id: number | null
+  id: number
+  description: string
+  fight: boolean
+  music_id: number
+  battlemaps: Battlemap | null
+  graphics_ground: Screen
+  graphics_wall: Screen
+  music: Music
 }
